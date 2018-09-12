9335210a9fc54ac04ec7378820a764ade5708567
dannysofftie <dankim761@gmail.com>
2018-09-12 14:58:20 +0300

    Add sample view

--- views/default.php
+++ views/default.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tekiria General Suppliers</title>
+    <title>PHP simple router</title>
 </head>
 
 <body>
-    Hello from Tekiria
+    PHP simple router, clone this repo and start your project.
 </body>
 
 </html>
